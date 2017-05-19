1df5e5a1f4cedf81e5205bc4128518f4ca89d3dc
i077 <imran3740@gmail.com>
2017-05-19 15:20:30 -0400

    Starting work on neural net

--- network.py
+++ network.py
@@ -1,11 +1,18 @@
-import tensorflow as tf
+import tflearn
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.estimator import regression
+
+LR = 1e-3 # Learning rate
+score_threshold = 5 # Number of lines needed to clear to learn
+initial_games = 10 # Number of games to train on
+
 # Reads board and returns as 1D list
 def readboard(board):
     new_board = []
     for i, row in enumerate(board[2:-1]):
         for j, cell in enumerate(row[1:-1]):
             if cell:
-                if(cell == 10):
+                if cell == 10 :
                     new_val = 1
                 else:
                     new_val = 0.5
@@ -13,3 +20,11 @@ def readboard(board):
                 new_val = 0
             new_board.append(new_val)
     return new_board
+
+# Returns a one_hot list with 1 at index one_index.
+def gen_onehot(one_index):
+    onehot = [0, 0, 0, 0, 0]
+    onehot[one_index] = 1
+    return onehot
+
+    
