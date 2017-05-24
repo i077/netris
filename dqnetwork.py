544a8b2622efa08a0c4f3cb2ee9c9007d12eb98a
i077 <imran3740@gmail.com>
2017-05-23 20:59:09 -0400

    Started work on deep Q network.

--- dqnetwork.py
+++ dqnetwork.py
@@ -1,6 +1,97 @@
+"""
+Using Deep-Q learning to train a machine to play Tetris.
+"""
 import tetris
+import tensorflow as tf
+import tflearn
+import numpy as np
 
+# Initialize game environment
 game = tetris.TetrisApp()
+num_actions = 5
+
+# TRAINING PARAMETERS
+# -------------------
+# Are we training or testing?
+test = False
+# Path to saved network model
+model_path = 'dqn_model.tflearn.ckpt'
+# Number of actor-learner threads
+n_threads = 8
+# Training steps
+T_MAX = 8e8
+# Current step
+T = 0
+# Size of minibatch when recalling from experience replay
+replay_minibatch = 4
+# Gradient update freq for each thread
+GradUpdate = 5
+# Step to reset target network
+Target = 40000
+# Learning rate
+learning_rate = 1e-3
+# Discount rate of future rewards
+y = 0.9
+# Number of steps to change ϵ to final val
+anneal_epsilon_timesteps = 4e5
+# Checkpoint data
+checkpoint_interval = 2000
+checkpoint_path = 'dqn_learning.tflearn.ckpt'
+
+# DEEP Q NETWORK
+# --------------
+# Building the Deep Q Network using TFLearn. The network should return
+# a list of q-values for each action.
+def build_model():
+    # Establish inputs
+    inputs = tf.placeholder(tf.float32, [None, 20, 10, replay_minibatch])
+    # Build network
+    net = tf.fully_connected(inputs, 128, activation='relu')
+    net = tf.fully_connected(inputs, 256, activation='relu')
+    net = tf.fully_connected(inputs, 128, activation='relu')
+    q_vals = tflearn.fully_connected(net, num_actions)
+    # Return inputs and q-values
+    return inputs, q_vals
+
+# Q-LEARNING
+# ----------
+# Determine final ϵ-value from list of possible values and probabilities.
+# Values taken from https://arxiv.org/pdf/1602.01783v1.pdf
+def get_final_epsilon():
+    final_vals = np.array([1, 0.01, 0.5])
+    probabilities = np.array([0.4, 0.3, 0.3])
+    return np.random_choice(final_vals, 1, p=list(probabilities))[0]
+
+# Async actor-learner thread, 1-step Q-learning
+def actor_learner_thread(thread_id, env, session, graph_ops, summary_ops, saver):
+    # Step counter and threshold should be global
+    global T_MAX, T
+
+def build_graph():
+    # Create a shared deep Q network
+    s, q_net = build_model()
+    network_params = tf.trainable_variables()
+    q_vals = q_net
+
+    # Create a target deep Q network
+    target_s, target_q_net = build_model()
+    target_network_params = tf.trainable_variables()[len(network_params):]
+    target_q_vals = target_q_net
+
+    # Periodically update target network weights
+    reset_target_network_params = \
+            [target_network_params[i].assign(network_params[i])
+             for i in range(len(target_network_params))]
+
+    # Cost and gradient operations
+    a = tf.placeholder("float", [None, num_actions])
+    y = tf.placeholder("float", [None])
+    action_q_vals = tf.reduce_sum(tf.multiply(q_vals, a), axis=1)
+    cost = tflearn.mean_square(action_q_vals, y)
+    optimizer = tf.train.RMSPropOptimizer(learning_rate)
+    update = optimizer.minimize(cost, var_list=network_params)
+
+    return s, q_vals, target_s, target_q_vals, reset_target_network_params, a, y, update
 
 if __name__ == '__main__':
     game.run()
